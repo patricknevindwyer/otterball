b14ee641b8a94dff5764a4080e1e423d3cbfd9dd
Patrick Dwyer <patricknevindwyer@gmail.com>
2016-08-07 17:26:50 -0400

    Added a location tracking full JSON parser

--- src/src/com/programmish/otterball/parsing/JSONParser.java
+++ src/src/com/programmish/otterball/parsing/JSONParser.java
@@ -3,6 +3,8 @@ package com.programmish.otterball.parsing;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JSONParser implements FingerPrintingParser {
 	private String content;
@@ -225,5 +227,307 @@ public class JSONParser implements FingerPrintingParser {
 		
 		return ranges;
 	}
+	
+	public List<ParsedElement> parse(String blob) {
+		return this.parse(blob, 0);
+	}
+	
+	/**
+	 * Run a full parse on the blob, returning a list of parsed
+	 * elements suitable for rebuilding the blob, condensing,
+	 * highlighting, etc.
+	 * 
+	 * @param blob
+	 * @param offset
+	 * @return List<ParsedElement> parse queue
+	 */
+	public List<ParsedElement> parse(String blob, int offset) {
+		String local = blob.substring(offset);
+		local = local.replaceAll("^\\s+", "");
 		
+		List<ParsedElement> elements = new ArrayList<>();
+		
+		int trimDiff = blob.length() - local.length() - offset;
+		
+		int elementAdjustment = trimDiff + offset + 1;
+		
+		// quick test for our outer marker
+		if ( !local.startsWith("{") && !local.startsWith("[")) {
+			return elements;
+		}
+		
+		// fast check for barewords
+		boolean inString = false;
+		
+		// create a quick lookup for nonbareword characters
+		char[] nonbare = {
+			':', ',', '[', ']', '{', '}', ' ', '\t', '\n',
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			'.', '-'
+		};
+		
+		HashMap<Character, Boolean> baremap = new HashMap<Character, Boolean>();
+		for (int i = 0; i < nonbare.length; i++) {
+			baremap.put(nonbare[i], true);
+		}
+		
+		// also create a lookup for numerics
+		char[] numerics = {
+				'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+				'-', '.'
+		};
+		
+		HashMap<Character, Boolean> numericmap = new HashMap<Character, Boolean>();
+		for (int i = 0; i < numerics.length; i++) {
+			numericmap.put(numerics[i], true);
+		}
+		
+		// number matching pattern for validating numerics
+		Pattern numericPattern = Pattern.compile("^-?[0-9]+(\\.[0-9]+)?$");
+		
+		// map which items pop which off of our stack - we'll use this for brace
+		// counting. Quotes manage themselves on the stack, because they
+		// have unique constraints
+		HashMap<Character, Character> stackMatches = new HashMap<>();
+		stackMatches.put('}', '{');
+		stackMatches.put(']',  '[');
+		
+		// which characters start the stack? - we use this to start the
+		// brace counting
+		HashMap<Character, Boolean> stackStarters = new HashMap<>();
+		stackStarters.put('{', true);
+		stackStarters.put('[', true);
+		
+		// our counting stack
+		List<Character> braceStack = new ArrayList<Character>();
+				
+		// let's begin.
+		boolean valid = true;
+		int pos = 0;
+		for (pos = 0; pos < local.length(); pos++) {
+			char c = local.charAt(pos);
+			
+			if (c == '"') {
+				// oh quotes, my old friend.
+				
+				if (local.charAt(pos - 1) == '\\') {
+					// this is an escape
+				}
+				else {
+					
+					if (!inString) {
+						braceStack.add('"');
+						
+						// we'll also add an element to the elements list for this
+						elements.add(new ParsedElement(ElementType.STRING, pos + elementAdjustment));
+						
+					}
+					else {
+						// we're poppin!
+						if ( (braceStack.size() > 0) && (braceStack.get(braceStack.size() - 1) == '"') ) {
+							// whew. it's good
+							braceStack.remove(braceStack.size() - 1);
+							
+							// we also need to mark the end of this string, thankfully it's the last element
+							// on the elements list
+							elements.get(elements.size() - 1).end = pos + elementAdjustment;
+							
+						}
+						else {
+							valid = false;
+							break;
+						}
+					}
+					
+					inString = !inString;					
+				}
+			}
+			else {
+				if (!inString) {
+					// is this an acceptable character?
+					
+					// do a look ahead for being a boolean
+					if (c == 't') {
+						if (local.substring(pos, pos + 4).equals("true")) {
+							// add to the element list
+							elements.add(new ParsedElement(ElementType.BOOLEAN, pos, pos + 3));
+
+							// ok, it's a bool, we jump
+							pos += 3;
+							
+							continue;
+						}
+					}
+					
+					if (c == 'f') {
+						if (local.substring(pos, pos + 5).equals("false")) {
+							// add to the parsed elements
+							elements.add(new ParsedElement(ElementType.BOOLEAN, pos, pos + 4));
+							
+							// woo
+							pos += 4;
+							continue;
+						}
+					}
+					
+					// check for the null string
+					if (c == 'n') {
+						if (local.substring(pos, pos + 4).equals("null")) {
+							// add to the element list
+							elements.add(new ParsedElement(ElementType.NULL, pos, pos + 3));
+							
+							// ok, it's the null string, we jump
+							pos += 3;
+							
+							continue;
+						}
+						
+					}
+					
+					// check the nonbare characters
+					if (baremap.containsKey(c)) {
+						
+						// are we poppin the stack?
+						if (stackStarters.containsKey(c)) {
+							
+							// we're starting a collection. what is it?
+							if (c == '{') {
+								elements.add(new ParsedElement(ElementType.OBJ_START, pos, pos));
+							}
+							else if (c == '[') {
+								elements.add(new ParsedElement(ElementType.LIST_START, pos, pos));
+							}
+							
+							// push it on the stack
+							braceStack.add(c);
+						}
+						else if (stackMatches.containsKey(c)) {
+							
+							// pop the stack
+							char m = braceStack.get(braceStack.size() - 1);
+							braceStack.remove(braceStack.size() - 1);
+							
+							if (stackMatches.get(c) != m) {
+								// we gots a problem
+								valid = false;
+								break;
+							}
+							
+							// what are we ending?
+							if (c == '}') {
+								elements.add(new ParsedElement(ElementType.OBJ_END, pos, pos));
+							}
+							else if (c == ']') {
+								elements.add(new ParsedElement(ElementType.LIST_END, pos, pos));
+							}
+							
+							// is our stack empty? If so, we've got a match
+							if (braceStack.size() == 0) {
+								return elements;
+							}
+						}
+						
+						// time to check our delimiters
+						else if (c == ':') {
+							
+							// make sure the element just before us is a string
+							ParsedElement look_behind = elements.get(elements.size() - 1);
+							
+							if (look_behind.type == ElementType.STRING) {
+								elements.add(new ParsedElement(ElementType.OBJ_SEPARATOR, pos, pos));
+							}
+							else {
+								// nerp.
+								valid = false;
+								break;
+							}
+						}
+						else if (c == ',') {
+							
+							// what was the previously parsed element
+							ParsedElement peek_back = elements.get(elements.size() - 1);
+							
+							// make sure the element just before us can support a delimiter
+							ElementType pbt = peek_back.type;
+							
+							if ( 
+									(pbt == ElementType.OBJ_START) || (pbt == ElementType.LIST_START) ||
+									(pbt == ElementType.OBJ_DELIMITER) || (pbt == ElementType.OBJ_SEPARATOR) ||
+									(pbt == ElementType.LIST_DELIMITER) ) {
+								// nada
+								valid = false;
+								break;
+							}
+							
+							// are we in a list or object?
+							char look_behind = braceStack.get(braceStack.size() - 1);
+							
+							if (look_behind == '{') {
+								elements.add(new ParsedElement(ElementType.OBJ_DELIMITER, pos, pos));
+							}
+							else if (look_behind == '[') {
+								elements.add(new ParsedElement(ElementType.LIST_DELIMITER, pos, pos));
+							}
+						}
+						
+						// now for numerics
+						else if (numericmap.containsKey(c)) {
+							
+							// we need to scan ahead and find the whole number, and then validate it
+							int look_ahead_idx = pos + 1;
+							
+							while (numericmap.containsKey(local.charAt(look_ahead_idx))) {
+								look_ahead_idx += 1;
+							}
+							
+							// pull out our number and validate it
+							String number = local.substring(pos, look_ahead_idx);
+							Matcher numberMatcher = numericPattern.matcher(number);
+							
+							System.out.println("Numeric is <" + number + ">");
+							
+							if (numberMatcher.matches()) {
+								
+								System.out.println("match");
+								// pop in on our element stack
+								elements.add(new ParsedElement(ElementType.NUMBER, pos, look_ahead_idx));
+								
+								// jump
+								pos = look_ahead_idx - 1;
+								continue;
+							}
+							else {
+								System.out.println("no match");
+								// we've got a problem. Number isn't sane.
+								valid = false;
+								break;
+							}
+							
+						}
+						
+						// Everything else should be white space 
+						// we're good
+						continue;
+					}
+										
+					// we've got a bare word
+					valid = false;
+					break;
+					
+				}
+				else {
+					// in a string, don't care
+				}
+			}
+		}
+		
+		if (valid && (braceStack.size() == 0)) {
+			// we've parsed successfully
+			return elements;
+		}
+		else {
+			return new ArrayList<ParsedElement>();
+		}
+		
+	}
 }
--- src/test/test/com/programmish/otterball/parsing/JSONParser_Test.java
+++ src/test/test/com/programmish/otterball/parsing/JSONParser_Test.java
@@ -1,13 +1,13 @@
 package test.com.programmish.otterball.parsing;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.List;
 
 import org.junit.Test;
 
 import com.programmish.otterball.parsing.JSONParser;
+import com.programmish.otterball.parsing.ParsedElement;
 import com.programmish.otterball.parsing.TextRange;
 
 public class JSONParser_Test {
@@ -45,5 +45,35 @@ public class JSONParser_Test {
 		assertTrue(ranges.size() == 2);
 		
 	}
-
+	
+	@Test
+	public void testParse() {
+		
+		JSONParser jp = new JSONParser();
+		
+		// start simple
+		List<ParsedElement> elements = jp.parse("{\"foo\": 3}");
+		assertTrue(elements.size() == 5);
+		
+		// harder number
+		elements = jp.parse("{\"a\": -3.14}");
+		assertTrue(elements.size() == 5);
+		
+		// step it up a bit
+		elements = jp.parse("{\"floob\": { \"a\": 1, \"b\": [1, 2.0, -3.4 ]}}");
+		this.dump(elements);
+		assertTrue(elements.size() > 0);
+		
+		// we need to make sure this is validating...
+		elements = jp.parse("{\"a\": [1 : , ,]}");
+		assertTrue(elements.size() == 0);
+		
+		
+	}
+	
+	protected void dump(List<ParsedElement> elements) {
+		for (ParsedElement e : elements) {
+			System.out.println(e.type + "\t\t(" + e.start + "," + e.end + ")");
+		}		
+	}
 }
